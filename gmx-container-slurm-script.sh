3d15ffbd9ca7e2202114d17a81463c0dbb549962
mkhpc <48019179+mkhpc@users.noreply.github.com>
2021-08-25 22:18:43 +0530

    Update gmx-container-slurm-script.sh

--- gmx-container-slurm-script.sh
+++ gmx-container-slurm-script.sh
@@ -8,4 +8,3 @@
 singularity run --nv /opt/apps/sif/gromacs_2021.sif gmx grompp -c conf.gro -p topol.top -f pme.mdp -o test.tpr
 
 singularity run --nv /opt/apps/sif/gromacs_2021.sif gmx mdrun -deffnm test -v -nsteps 10000 -nb gpu -pme gpu -ntmpi 1 -ntomp 8 -resethway -noconfout -pin on -noddcheck
-
