24a3e171b4dfec9edeb87fafda42061bee30f487
mkhpc <48019179+mkhpc@users.noreply.github.com>
2021-08-25 20:19:17 +0530

    Update tf-container-slurm-scriptv1.sh

--- tf-container-slurm-scriptv1.sh
+++ tf-container-slurm-scriptv1.sh
@@ -5,5 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=debug
 
+git clone https://github.com/tensorflow/benchmarks.git
+cd benchmarks/scripts/tf_cnn_benchmarks
 singularity run --nv /opt/apps/sif/tensorflow_21.07-tf2-py3.sif python tf_cnn_benchmarks.py
 
